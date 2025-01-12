39b043159f4c4c2cc3ca1d74142d90243b8f84ad
ironnicko <nickonicko779@gmail.com>
2025-01-12 11:05:10 +0530

    upd: added quic

--- Segment/api/main.py
+++ Segment/api/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 from .dependencies import lifespan
 from .routers import outfits, clothing
 
@@ -29,7 +28,3 @@ app.add_middleware(
 
 app.include_router(outfits.router)
 app.include_router(clothing.router)
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
--- Segment/main.py
+++ Segment/main.py
@@ -1,5 +1,14 @@
-import uvicorn
+import asyncio
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
 from api.main import app
 
+async def main():
+    config = Config()
+    config.bind = ["0.0.0.0:8001"]
+    config.quic_bind = ["0.0.0.0:8001"]
 
-uvicorn.run(app, host="0.0.0.0", port=8001)
+    await serve(app, config)
+if __name__ == "__main__":
+    asyncio.run(main())
+
