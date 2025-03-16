cae52ea32742e747544f3db2013efa80a0358c73
ironnicko <nickonicko779@gmail.com>
2025-03-17 02:03:09 +0530

    upd: user model

--- Go-Backend/go.mod
+++ Go-Backend/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 	github.com/supabase-community/auth-go v1.3.2
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
--- Go-Backend/models/userModel.go
+++ Go-Backend/models/userModel.go
@@ -3,14 +3,24 @@ package models
 import (
 	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	ID                    uuid.UUID `gorm:"type:uuid;uniqueIndex;primaryKey;default:uuid_generate_v4()" json:"id"`
-	Username              string    `gorm:"size:255;uniqueIndex" json:"username"`
-	IsOnBoardingCompleted bool      `gorm:"type:bool;default:false" json:"IsOnBoardingCompleted"`
+	ID                    uuid.UUID      `gorm:"type:uuid;uniqueIndex;primaryKey;default:uuid_generate_v4()" json:"id"`
+	Username              string         `gorm:"size:255;uniqueIndex" json:"username"`
+	IsOnBoardingCompleted bool           `gorm:"type:bool;default:false" json:"isOnboardingCompleted"`
+	MarketOnBoarding      bool           `gorm:"type:bool;default:false" json:"marketOnBoarding"`
+	Gender                string         `gorm:"size:7" json:"gender"`
+	BodyShape             string         `gorm:"size:255" json:"bodyShape"`
+	DressingChallenges    pq.StringArray `gorm:"type:text[];" json:"dressingChallenges"`
+	Colors                pq.StringArray `gorm:"type:text[];" json:"colors"`
+	Styles                pq.StringArray `gorm:"type:text[];" json:"styles"`
+	SkinTone              string         `gorm:"type:text[];size:255" json:"skinTone"`
+	UnderTone             string         `gorm:"type:text[];size:255" json:"underTone"`
+	BodyImages            pq.StringArray `gorm:"type:text[];" json:"bodyImages"`
 }
 
 func (u *User) Validate() error {
